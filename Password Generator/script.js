1848ce8d44d47d5e9c81eb2177ecf29ddb914422
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-10-23 20:41:07 +0530

    password generator completed

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -6,6 +6,8 @@ let lowercase = document.getElementById("lowercase");
 let uppercase = document.getElementById("uppercase");
 let numbers = document.getElementById("numbers");
 let symbols = document.getElementById("symbols");
+let copyIcon = document.getElementById("copyIcon");
+
 
 rangeValue.innerText = rangeBox.value;
 rangeBox.addEventListener("input", () => {
@@ -15,7 +17,42 @@ genBtn.addEventListener("click", () => {
     passBox.value = generatePassword();
 });
 
-let lowerChars = "abcdefghijklmnopqrstuvwxyz";
 let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
 let allNumbers = "0123456789";
 let allSymbols = "~!@#$%^&*";
+
+function generatePassword() {
+    let genPassword = "";
+    let allChars = "";
+
+    allChars += lowercase.checked ? lowerChars : "";
+    allChars += uppercase.checked ? upperChars : "";
+    allChars += numbers.checked ? allNumbers : "";
+    allChars += symbols.checked ? allSymbols : "";
+
+    if(allChars === "" || allChars.length == 0) {
+        return genPassword;
+    }
+
+    let i = 1;
+    while(i <= rangeBox.value) {
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+    return genPassword; 
+}
+
+copyIcon.addEventListener("click", () => {
+
+    if(passBox.value != "" || passBox.value.length >=1){
+        navigator.clipboard.writeText(passBox.value);
+        copyIcon.innerText = "";
+        alert("Password Copied");
+
+        SetTimeout(() => {
+            copyIcon.innerText = "content_copy";
+            copyIcon.title ="";
+        }, 2000)
+    }
+});
