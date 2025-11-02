65142bdd49d2b4ed9578f80a1b1ff23f2ddcb662
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-11-02 17:20:17 +0530

    color flipper completed

--- Color Flipper/script.js
+++ Color Flipper/script.js
@@ -8,6 +8,7 @@ function randomColor(){
     const yellow = Math.round(Math.random() * 255);
     const red = Math.round(Math.random() * 255);
     const orange = Math.round(Math.random() * 255);
+
     const color = `rgb(${yellow}, ${red}, ${orange})`;
     body.style.backgroundColor = color;
 }
