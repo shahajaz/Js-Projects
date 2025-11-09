948c04481edd2942662d252a67f1e5de20564495
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-11-09 14:31:01 +0530

    stop watch

--- Stop Watch/script.js
+++ Stop Watch/script.js
@@ -5,3 +5,18 @@ const resetBtn = document.getElementById('.reset');
 startBtn.addEventListener('click', startTimer);
 stopBtn.addEventListener('click', stopTimer);
 resetBtn.addEventListener('click', resetTimer);
+
+function start(){
+    startBtn.classList.add("active");
+    startBtn.classList.remove("stopActive");
+}
+
+function stop(){
+    stopBtn.classList.add("active");
+    startBtn.classList.remove("stopActive");
+}
+
+function reset(){
+    resetBtn.classList.add("active");
+    resetBtn.classList.remove("stopActive");
+}
