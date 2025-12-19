1d55517fc331d9b86be87f2ec445645b236b4324
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-12-19 23:40:34 +0530

    E Commerce navbar design

--- E-Commerce/script.js
+++ E-Commerce/script.js
@@ -1,7 +1,7 @@
-// Mobile menu toggle
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
+
