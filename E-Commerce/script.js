633d2c316b1b5c0c58cc2da382611e8fcde129c0
ajaz-mohammad <ajazmohammad2003@gmail.com>
2025-12-20 23:13:50 +0530

    hero section

--- E-Commerce/script.js
+++ E-Commerce/script.js
@@ -5,3 +5,37 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let currentIndex = 0;
+
+function updateCarousel(index) {
+  track.style.transform = `translateX(-${index * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+}
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel(currentIndex);
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex =
+    (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel(currentIndex);
+});
+
+dots.forEach(dot => {
+  dot.addEventListener("click", () => {
+    currentIndex = Number(dot.dataset.slide);
+    updateCarousel(currentIndex);
+  });
+});
+
